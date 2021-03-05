f9f178971ba74453ab2da7c3b0bc8a2d1c1391b5
João Rodrigues <80014156+Joao-Ex-Machina@users.noreply.github.com>
2021-03-05 12:29:01 +0000

    Add files via upload

---
+++ Exercises/Prob2_2018-2019/Prob1.c
@@ -0,0 +1,27 @@
+/* Exercise 1 - Print all divisors of a scanned integer and then print the average of the divisors
+Solution by : Joao Rodrigues (Joao-Ex-Machina)
+March 2021 */
+
+#include <stdio.h>
+#include <stdlib.h>
+int n, a, sum, size, flag;
+float avrg;
+
+
+int main(){
+    size = 0;
+    sum = 0;
+
+    printf ("Insert number:");
+   scanf("%d", &n);
+  for ( a = 1; a <= n; a = ++ a ){
+      flag = n%a;
+      if (flag == 0){
+          printf ("\n %d is a divisor", a);
+          sum = sum + a; // If a is a divisor its value is "stored" in sum of all previous divisors
+          ++ size; // If a is a divisor the size of elements in the sum increments by one
+      }
+  }
+  avrg = (float) sum/size;
+  printf ("\n %.3f is the average value of the divisors", avrg);
+}
